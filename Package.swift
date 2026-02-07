083c25d0e0fa2fc07079d0c13de2d79f93ffebf7
quiclane <developer@veridanlayerseven.com>
2026-02-07 22:43:41 +0200

    spm: tools 6.2 + update binary url/checksum

--- Package.swift
+++ Package.swift
@@ -1,3 +1,3 @@
-// swift-tools-version:5.9
+// swift-tools-version: 6.2
 import PackageDescription
-let package=Package(name:"lsquic-spm",products:[.library(name:"LSQUIC",targets:["LSQUIC"])],targets:[.binaryTarget(name:"LSQUIC",url:"https://github.com/quiclane/lsquic-spm/releases/download/v0.0.1/LSQUIC.xcframework.zip",checksum:"22c2760f278026f3c2cb60f6160336168fa76ffa720c88a8208aa4634981d0dc")])
+let package=Package(name:"lsquic-spm",platforms:[.iOS(.v13)],products:[.library(name:"LSQUIC",targets:["LSQUIC"])],targets:[.binaryTarget(name:"LSQUIC",url:"https://github.com/quiclane/lsquic-spm/releases/download/ios-20260208-9e7c2ba0f865/LSQUIC.xcframework.zip",checksum:"b8af1f54906b28e8a4eca3cc819f878062dbb95cba92185a407409df6ed93aa8")])
