b34005b19d806965904e8ba755c0afed9c74ef62
quiclane <developer@veridanlayerseven.com>
2026-02-08 03:08:23 +0200

    spm: use path binaryTarget (repo-root LSQUIC.xcframework)

--- Package.swift
+++ Package.swift
@@ -1,3 +1,3 @@
 // swift-tools-version: 6.2
 import PackageDescription
-let package=Package(name:"lsquic-spm",platforms:[.iOS(.v13)],products:[.library(name:"LSQUIC",targets:["LSQUIC"])],targets:[.binaryTarget(name:"LSQUIC",url:"https://github.com/quiclane/lsquic-spm/releases/download/ios-20260208-9e7c2ba0f865/LSQUIC.xcframework.zip",checksum:"b8af1f54906b28e8a4eca3cc819f878062dbb95cba92185a407409df6ed93aa8")])
+let package=Package(name:"lsquic-spm",platforms:[.iOS(.v13)],products:[.library(name:"LSQUIC",targets:["LSQUIC"])],targets:[.binaryTarget(name:"LSQUIC",path:"LSQUIC.xcframework")])
